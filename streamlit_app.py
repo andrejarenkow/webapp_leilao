8186c82f8e29c90dee040bd6c87438dd8092f30e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-13 15:11:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if submitted:
    
    page = 0
    #for i in range(1,100,1):
-   while len(soup('p',{'class':'product-price-bid'}))>0:
+   while len(soup('div',{'class':'visits'}))>0:
        page += 1
        print(page)
        num_leilao = link_leilao.split('Num=')[1]
