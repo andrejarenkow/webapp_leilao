d656d47a5d8ef9aa8a8e96e3285509f4710e7056
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-13 00:36:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -225,8 +225,8 @@ if submitted:
   dados_precos = pd.pivot_table(total_historico_valores, index='peca', values='valor', aggfunc=['max', 'count']).reset_index()
   dados_precos
   dados = dados.merge(dados_precos, left_on='id', right_on='peca', how='left')
-  dados['lancado'] = dados['(count, valor)'].apply(lambda x: 1 if x > 0 else 0)
-  dados['valor_vendido'] = dados['lancado']*dados['(max, valor)']
+  dados['lancado'] = dados['count'].apply(lambda x: 1 if x > 0 else 0)
+  dados['valor_vendido'] = dados['lancado']*dados['max']
   dados.sort_values([ 'valor_vendido' ], ascending=False, inplace=True)
 
   
@@ -234,19 +234,19 @@ if submitted:
   col1, col2, col3 = st.columns([1,2,3])
    
   with col3:
-       st.dataframe(dados[['imagem','descrição','(max, valor)','(count, valor)','visitas','links']].fillna('Lote nào vendido'),hide_index=True,
+       st.dataframe(dados[['imagem','descrição','max','count','visitas','links']].fillna('Lote nào vendido'),hide_index=True,
                     use_container_width=True,
                     height=600,
                    column_config={
                    'descrição':st.column_config.TextColumn(width='medium'),
                    'imagem':st.column_config.ImageColumn(),
                    'links':st.column_config.LinkColumn(),
-                   '(max, valor)':st.column_config.NumberColumn(
+                   'max':st.column_config.NumberColumn(
                      'Preço vendido',
                      format="R$%.2f",
                      width='small'
                    ),
-                     '(count, valor)':st.column_config.NumberColumn(
+                     'count':st.column_config.NumberColumn(
                      'Lances',
                      width='small'
                    ),
